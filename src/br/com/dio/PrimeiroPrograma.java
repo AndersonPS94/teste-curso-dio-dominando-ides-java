579e6226bf67181d6c96dc922a736742805d66e8
AndersonPS94 <dson_adn@hotmail.com>
2022-02-16 22:54:19 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+				
+		System.out.println("Hello Wolrd! " +(a+b));
+
+	}
+
+}
